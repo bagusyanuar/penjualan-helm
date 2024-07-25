8a6e87a04dfccf438e44c5d70c759f148682a32b
bagusyanuar <bagus.yanuar613@gmail.com>
2024-07-25 23:40:51 +0700

    category api

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,78 @@
+<?php
+
+
+namespace App\Http\Controllers\Api;
+
+
+use App\Helper\CustomController;
+use App\Models\Customer;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends CustomController
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function register()
+    {
+        try {
+            DB::beginTransaction();
+            $username = $this->postField('username');
+            $password = Hash::make($this->postField('password'));
+            $name = $this->postField('name');
+            $phone = $this->postField('phone');
+
+            $data_user = [
+                'username' => $username,
+                'password' => $password,
+                'role' => 'customer'
+            ];
+
+            $user = User::create($data_user);
+
+            $data_customer = [
+                'user_id' => $user->id,
+                'name' => $name,
+                'phone' => $phone
+            ];
+
+            Customer::create($data_customer);
+            DB::commit();
+            return $this->jsonSuccessResponse('success');
+        }catch (\Exception $e) {
+            DB::rollBack();
+            return $this->jsonErrorResponse($e->getMessage());
+        }
+    }
+
+    public function login()
+    {
+        try {
+            $username = $this->postField('username');
+            $password = $this->postField('password');
+
+            $user = User::with([])
+                ->where('username', '=', $username)
+                ->first();
+            if (!$user) {
+                return $this->jsonNotFoundResponse('user not found!');
+            }
+
+            $isPasswordValid = Hash::check($password, $user->password);
+            if (!$isPasswordValid) {
+                return $this->jsonUnauthorizedResponse('username and password did not match...');
+            }
+
+            $token = auth('api')->setTTL(null)->tokenById($user->id);
+            return $this->jsonSuccessResponse('success', [
+                'access_token' => $token
+            ]);
+        } catch (\Exception $e) {
+            return $this->jsonErrorResponse($e->getMessage());
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/', function () {
+    return response()->json([
+        'app_name' => 'sadean_helm_app',
+        'app_version' => 'v1.0'
+    ], 200);
+});
+
+Route::post('/register', [\App\Http\Controllers\API\AuthController::class, 'register']);
+Route::post('/login', [\App\Http\Controllers\API\AuthController::class, 'login']);
+
+//Ro
+Route::group(['middleware' => ['jwt.verify']], function () {
+
+    Route::group(['prefix' => 'category'], function () {
+        Route::get('/', [\App\Http\Controllers\API\CategoriesController::class, 'index']);
+        Route::get('/{id}', [\App\Http\Controllers\API\CategoriesController::class, 'findByID']);
+    });
 });
